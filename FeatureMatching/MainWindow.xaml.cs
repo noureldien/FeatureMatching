b87f7869988beb1386b769f0159e00213f39b22c
noureldien <nreldien@yahoo.com>
2015-02-07 21:43:48 +0000

    Commit

--- FeatureMatching/MainWindow.xaml.cs
+++ FeatureMatching/MainWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using OpenCvSharp;
-using OpenCvSharp.CPlusPlus;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,67 +18,36 @@ namespace FeatureMatching
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : System.Windows.Window
+    public partial class MainWindow : Window
     {
+        #region Private Variables
+        
+        private Tracker tracker;
+
+        #endregion
+
+        #region Constructor
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        
+        #endregion
 
-        private void Sift()
+        #region Event Handlers
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Mat src1 = new Mat("image1.png", LoadMode.GrayScale);
-            Mat src2 = new Mat("image2.png", LoadMode.GrayScale);
-
-            // Detect the keypoints and generate their descriptors using SIFT
-            SIFT sift = new SIFT();
-
-            KeyPoint[] keypoints1, keypoints2;
-            MatOfFloat descriptors1 = new MatOfFloat();
-            MatOfFloat descriptors2 = new MatOfFloat();
-            sift.Run(src1, null, out keypoints1, descriptors1);
-            sift.Run(src2, null, out keypoints2, descriptors2);
-
-            // Matching descriptor vectors with a brute force matcher
-            BFMatcher matcher = new BFMatcher(NormType.L2, false);
-            DMatch[] matches = matcher.Match(descriptors1, descriptors2);
-
-            // Draw matches
-            Mat view = new Mat();
-            Cv2.DrawMatches(src1, keypoints1, src2, keypoints2, matches, view);
-
-            using (new OpenCvSharp.CPlusPlus.Window("SIFT matching", WindowMode.AutoSize, view))
-            {
-                Cv2.WaitKey();
-            }
+            tracker = new Tracker(labelFrameCounter);
+            tracker.InitilizeCamera();
+            tracker.StartProcessing();
         }
 
-        private void Surf()
-        {
-            Mat src1 = new Mat("image1.png", LoadMode.GrayScale);
-            Mat src2 = new Mat("image2.png", LoadMode.GrayScale);
+        #endregion
 
-            // Detect the keypoints and generate their descriptors using SURF
-            SURF surf = new SURF(500, 4, 2, true);
+        #region Private Methods
 
-            KeyPoint[] keypoints1, keypoints2;
-            MatOfFloat descriptors1 = new MatOfFloat();
-            MatOfFloat descriptors2 = new MatOfFloat();
-            surf.Run(src1, null, out keypoints1, descriptors1);
-            surf.Run(src2, null, out keypoints2, descriptors2);
-
-            // Matching descriptor vectors with a brute force matcher
-            BFMatcher matcher = new BFMatcher(NormType.L2, false);
-            DMatch[] matches = matcher.Match(descriptors1, descriptors2);
-
-            // Draw matches
-            Mat view = new Mat();
-            Cv2.DrawMatches(src1, keypoints1, src2, keypoints2, matches, view);
-
-            using (new OpenCvSharp.CPlusPlus.Window("SURF matching", WindowMode.AutoSize, view))
-            {
-                Cv2.WaitKey();
-            }
-        }
+        #endregion
     }
 }
